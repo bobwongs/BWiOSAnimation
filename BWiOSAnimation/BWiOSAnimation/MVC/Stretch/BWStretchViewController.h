70d1c34d6ae5158f9a8f44fcb4012dac7ca6e50c
wangbo <wangbo@bluemoon.com.cn>
2016-10-31 17:35:53 +0800

    Modify

---
+++ BWiOSAnimation/BWiOSAnimation/MVC/Stretch/BWStretchViewController.h
@@ -0,0 +1,13 @@
+//
+//  BWStretchViewController.h
+//  BWiOSAnimation
+//
+//  Created by BobWong on 16/10/31.
+//  Copyright © 2016年 BobWongStudio. All rights reserved.
+//
+
+#import "BWBaseViewController.h"
+
+@interface BWStretchViewController : BWBaseViewController
+
+@end
---
+++ BWiOSAnimation/BWiOSAnimation/MVC/Stretch/BWStretchViewController.m
@@ -0,0 +1,22 @@
+//
+//  BWStretchViewController.m
+//  BWiOSAnimation
+//
+//  Created by BobWong on 16/10/31.
+//  Copyright © 2016年 BobWongStudio. All rights reserved.
+//
+
+#import "BWStretchViewController.h"
+
+@interface BWStretchViewController ()
+
+@end
+
+@implementation BWStretchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
