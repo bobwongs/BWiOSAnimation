29def4a92058de2f815063e7f3603e4e3d6d1b49
BobWong <wangbo@bluemoon.com.cn>
2017-04-24 13:49:06 +0800

    Modift

--- BWiOSAnimation/BWiOSAnimation/MVC/CoreAnimation/Controller/BWCoreAnimationViewController.m
+++ BWiOSAnimation/BWiOSAnimation/MVC/CoreAnimation/Controller/BWCoreAnimationViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     
 //    [self animateSimpleChanges];
+//    [self usingKeyframeAnimation];
+    [self animateMultipleChangesTogether];
 }
 
 - (void)animateSimpleChanges {
@@ -39,6 +41,64 @@
     // Change the actual data value in the layer to the final value.
     layer0.opacity = 0.0;
     
+}
+
+- (void)usingKeyframeAnimation {
+    CALayer *superLayer = self.view.layer;
+    
+    CALayer *layer0 = [CALayer new];
+    layer0.frame = CGRectMake(74, 74, 50, 50);
+    layer0.backgroundColor = [UIColor greenColor].CGColor;
+    [superLayer addSublayer:layer0];
+    
+    // create a CGPath that implements two arcs (a bounce)
+    CGMutablePathRef thePath = CGPathCreateMutable();
+    CGPathMoveToPoint(thePath,NULL,74.0,74.0);
+    CGPathAddCurveToPoint(thePath,NULL,74.0,500.0,
+                          320.0,500.0,
+                          320.0,74.0);
+    CGPathAddCurveToPoint(thePath,NULL,320.0,500.0,
+                          566.0,500.0,
+                          566.0,74.0);
+    
+    CAKeyframeAnimation *theAnimation;
+    
+    // Create the animation object, specifying the position property as the key path.
+    theAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    theAnimation.path = thePath;
+    theAnimation.duration = 5.0;
+    
+    // Add the animation to the layer.
+    [layer0 addAnimation:theAnimation forKey:@"position"];
+}
+
+- (void)animateMultipleChangesTogether {
+    CALayer *superLayer = self.view.layer;
+    
+    CALayer *layer0 = [CALayer new];
+    layer0.frame = CGRectMake(74, 74, 50, 50);
+    layer0.backgroundColor = [UIColor greenColor].CGColor;
+    [superLayer addSublayer:layer0];
+    
+    // Animation 1
+    CAKeyframeAnimation* widthAnim = [CAKeyframeAnimation animationWithKeyPath:@"borderWidth"];
+    NSArray* widthValues = [NSArray arrayWithObjects:@1.0, @10.0, @5.0, @30.0, @0.5, @15.0, @2.0, @50.0, @0.0, nil];
+    widthAnim.values = widthValues;
+    widthAnim.calculationMode = kCAAnimationPaced;
+    
+    // Animation 2
+    CAKeyframeAnimation* colorAnim = [CAKeyframeAnimation animationWithKeyPath:@"borderColor"];
+    NSArray* colorValues = [NSArray arrayWithObjects:(id)[UIColor greenColor].CGColor,
+                            (id)[UIColor redColor].CGColor, (id)[UIColor blueColor].CGColor,  nil];
+    colorAnim.values = colorValues;
+    colorAnim.calculationMode = kCAAnimationPaced;
+    
+    // Animation group
+    CAAnimationGroup* group = [CAAnimationGroup animation];
+    group.animations = [NSArray arrayWithObjects:colorAnim, widthAnim, nil];
+    group.duration = 5.0;
+    
+    [layer0 addAnimation:group forKey:@"BorderChanges"];
 }
 
 @end
